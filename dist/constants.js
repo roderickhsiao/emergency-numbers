376b5d9e3d0397036c58f9f95b2a1e49204e0d33
roderickhsiao <roderickhsiao@users.noreply.github.com>
2023-06-30 19:20:06 +0000

    deploy: 9005589e294a8bf4fe9230e0306ac6d70f06a2d0

---
+++ dist/constants.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NumberType = void 0;
+var NumberType;
+(function (NumberType) {
+    NumberType["FIRE"] = "fire";
+    NumberType["POLICE"] = "police";
+    NumberType["AMBULANCE"] = "ambulance";
+})(NumberType || (exports.NumberType = NumberType = {}));
+;
+//# sourceMappingURL=constants.js.map
