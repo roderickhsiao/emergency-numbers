549a0cc6dfc352b216e9deb99c26f9978b11cf8a
roderickhsiao <roderickhsiao@users.noreply.github.com>
2023-06-30 19:29:10 +0000

    deploy: 5a1de2ef9b0949f676fe466daa2a81dce7626ea3

--- dist/constants.js
+++ dist/constants.js
@@ -1,11 +1,21 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NumberType = void 0;
-var NumberType;
-(function (NumberType) {
-    NumberType["FIRE"] = "fire";
-    NumberType["POLICE"] = "police";
-    NumberType["AMBULANCE"] = "ambulance";
-})(NumberType || (exports.NumberType = NumberType = {}));
-;
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.NumberType = void 0;
+    var NumberType;
+    (function (NumberType) {
+        NumberType["FIRE"] = "fire";
+        NumberType["POLICE"] = "police";
+        NumberType["AMBULANCE"] = "ambulance";
+    })(NumberType || (exports.NumberType = NumberType = {}));
+    ;
+});
 //# sourceMappingURL=constants.js.map
--- dist/public/script.js
+++ dist/public/script.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -35,77 +34,88 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-// @ts-expect-error
-var country_code_emoji_1 = require("country-code-emoji");
-var index_1 = require("../index");
-// Function to generate table rows
-function createTableRow(countryCode, countryName, emergencyNumbers) {
-    var row = document.createElement('tr');
-    var flagCell = document.createElement('td');
-    flagCell.innerHTML = (0, country_code_emoji_1.countryCodeEmoji)(countryCode);
-    row.appendChild(flagCell);
-    var codeCell = document.createElement('td');
-    codeCell.textContent = countryCode;
-    row.appendChild(codeCell);
-    var nameCell = document.createElement('td');
-    nameCell.textContent = countryName;
-    row.appendChild(nameCell);
-    var fireCell = document.createElement('td');
-    fireCell.textContent = emergencyNumbers.fire;
-    row.appendChild(fireCell);
-    var policeCell = document.createElement('td');
-    policeCell.textContent = emergencyNumbers.police;
-    row.appendChild(policeCell);
-    var ambulanceCell = document.createElement('td');
-    ambulanceCell.textContent = emergencyNumbers.ambulance;
-    row.appendChild(ambulanceCell);
-    return row;
-}
-// Function to generate the table
-function generateTable() {
-    return __awaiter(this, void 0, void 0, function () {
-        var table, tableHeader, headerRow, headers, tableBody, countries, _i, countries_1, countryCode, countryMetadata, countryName, row;
-        return __generator(this, function (_a) {
-            table = document.createElement('table');
-            table.className = 'table-auto mx-auto';
-            tableHeader = table.createTHead();
-            headerRow = tableHeader.insertRow();
-            headers = ['Flag', 'Country Code', 'Country Name', 'Fire', 'Police', 'Ambulance'];
-            headers.forEach(function (headerText) {
-                var headerCell = document.createElement('th');
-                headerCell.className = 'px-4 py-2 bg-gray-200';
-                headerCell.textContent = headerText;
-                headerRow.appendChild(headerCell);
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports", "country-code-emoji", "../index"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    // @ts-expect-error
+    var country_code_emoji_1 = require("country-code-emoji");
+    var index_1 = require("../index");
+    // Function to generate table rows
+    function createTableRow(countryCode, countryName, emergencyNumbers) {
+        var row = document.createElement('tr');
+        var flagCell = document.createElement('td');
+        flagCell.innerHTML = (0, country_code_emoji_1.countryCodeEmoji)(countryCode);
+        row.appendChild(flagCell);
+        var codeCell = document.createElement('td');
+        codeCell.textContent = countryCode;
+        row.appendChild(codeCell);
+        var nameCell = document.createElement('td');
+        nameCell.textContent = countryName;
+        row.appendChild(nameCell);
+        var fireCell = document.createElement('td');
+        fireCell.textContent = emergencyNumbers.fire;
+        row.appendChild(fireCell);
+        var policeCell = document.createElement('td');
+        policeCell.textContent = emergencyNumbers.police;
+        row.appendChild(policeCell);
+        var ambulanceCell = document.createElement('td');
+        ambulanceCell.textContent = emergencyNumbers.ambulance;
+        row.appendChild(ambulanceCell);
+        return row;
+    }
+    // Function to generate the table
+    function generateTable() {
+        return __awaiter(this, void 0, void 0, function () {
+            var table, tableHeader, headerRow, headers, tableBody, countries, _i, countries_1, countryCode, countryMetadata, countryName, row;
+            return __generator(this, function (_a) {
+                table = document.createElement('table');
+                table.className = 'table-auto mx-auto';
+                tableHeader = table.createTHead();
+                headerRow = tableHeader.insertRow();
+                headers = ['Flag', 'Country Code', 'Country Name', 'Fire', 'Police', 'Ambulance'];
+                headers.forEach(function (headerText) {
+                    var headerCell = document.createElement('th');
+                    headerCell.className = 'px-4 py-2 bg-gray-200';
+                    headerCell.textContent = headerText;
+                    headerRow.appendChild(headerCell);
+                });
+                tableBody = table.createTBody();
+                countries = (0, index_1.getSupportedCountries)();
+                for (_i = 0, countries_1 = countries; _i < countries_1.length; _i++) {
+                    countryCode = countries_1[_i];
+                    countryMetadata = (0, index_1.getEmergencyNumberByCountry)(countryCode);
+                    countryName = new Intl.DisplayNames(['en'], { type: 'region' }).of(countryCode);
+                    row = createTableRow(countryCode, countryName, countryMetadata);
+                    tableBody.appendChild(row);
+                }
+                return [2 /*return*/, table];
             });
-            tableBody = table.createTBody();
-            countries = (0, index_1.getSupportedCountries)();
-            for (_i = 0, countries_1 = countries; _i < countries_1.length; _i++) {
-                countryCode = countries_1[_i];
-                countryMetadata = (0, index_1.getEmergencyNumberByCountry)(countryCode);
-                countryName = new Intl.DisplayNames(['en'], { type: 'region' }).of(countryCode);
-                row = createTableRow(countryCode, countryName, countryMetadata);
-                tableBody.appendChild(row);
-            }
-            return [2 /*return*/, table];
         });
-    });
-}
-// Display the table
-function displayTable() {
-    var _a;
-    return __awaiter(this, void 0, void 0, function () {
-        var emergencyNumbersTable;
-        return __generator(this, function (_b) {
-            switch (_b.label) {
-                case 0: return [4 /*yield*/, generateTable()];
-                case 1:
-                    emergencyNumbersTable = _b.sent();
-                    (_a = document.getElementById('emergency-numbers')) === null || _a === void 0 ? void 0 : _a.appendChild(emergencyNumbersTable);
-                    return [2 /*return*/];
-            }
+    }
+    // Display the table
+    function displayTable() {
+        var _a;
+        return __awaiter(this, void 0, void 0, function () {
+            var emergencyNumbersTable;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0: return [4 /*yield*/, generateTable()];
+                    case 1:
+                        emergencyNumbersTable = _b.sent();
+                        (_a = document.getElementById('emergency-numbers')) === null || _a === void 0 ? void 0 : _a.appendChild(emergencyNumbersTable);
+                        return [2 /*return*/];
+                }
+            });
         });
-    });
-}
-displayTable();
+    }
+    displayTable();
+});
 //# sourceMappingURL=script.js.map
